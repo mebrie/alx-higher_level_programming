9f8e0020adfb0e5689bb61aab665e682d765b4e8
mebrie <mebrieka@gmail.com>
2022-08-02 12:46:54 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-import sys
 
+"""
+Module that reads stdin line by line and computes metrics
+"""
 
-def print_info():
-    print('File size: {:d}'.format(file_size))
+if __name__ == '__main__':
 
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
+    import sys
 
+    file_size = 0
+    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {i: 0 for i in possible_codes}
+    counter = 0
 
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_size))
+        for i, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(i, v))
 
-lc = 0
-file_size = 0
-
-try:
-    for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
-
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
-
-        lc += 1
-
-    print_info()
-except KeyboardInterrupt:
-    print_info()
-    raise
+    try:
+        for line in sys.stdin:
+            counter += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_stats(stats, file_size)
+        print_stats(stats, file_size)
+    except KeyboardInterrupt:
+        print_stats(stats, file_size)
+        raise
